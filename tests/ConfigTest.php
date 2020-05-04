569576498ab4de5ba23af639173cde99754ba5f4
janoschreuschenbach <j.reuschenbach@onofice.com>
2020-05-04 20:37:02 +0200

    coding style

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -5,12 +5,12 @@ use jr\ooapi\Config;
 
 class ConfigTest extends TestCase
 {
-    public function testInstance()
+    public function testInstance(): void
     {
         $this->assertInstanceOf(Config::class, new Config('config/ooapi.ini'));
     }
 
-    public function testGetApiUrl()
+    public function testGetApiUrl(): void
     {
         $pConfig = new Config('config/ooapi.ini');
         $this->assertEquals('https://api.onoffice.de/api/latest/api.php', $pConfig->getApiUrl());
