29b04d9816969ef39d9366eb483195f09b4aba30
jreuschenbach <j.reuschenbach@onOffice.com>
2020-05-28 21:34:25 +0200

    gui: check if credentials exist
     * hide token/secret-inputs

--- public/checkCredentialStorage.php
+++ public/checkCredentialStorage.php
@@ -3,3 +3,8 @@
 namespace jr\ooapi;
 
 include(__DIR__.'/../vendor/autoload.php');
+
+$config = new Config(__DIR__ . '/../config/ooapi.ini');
+$credentialStorage = new CredentialStorage($config->getCredentialDir());
+
+echo $credentialStorage->isSomethingStored();
