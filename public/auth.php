c25e469a7fbc332943a975ec03c1a5137c2c68dc
jreuschenbach <j.reuschenbach@onOffice.com>
2020-05-29 10:14:49 +0200

    gui: error-handling on empty passwords

--- public/auth.php
+++ public/auth.php
@@ -9,19 +9,29 @@ $token = array_key_exists('token', $_POST) ? $_POST['token'] : null;
 $secret = array_key_exists('secret', $_POST) ? $_POST['secret'] : null;
 $password = array_key_exists('password', $_POST) ? $_POST['password'] : null;
 
-if (is_string($token) && is_string($secret) && is_string($password))
-{
-    $credentials = new Credentials($token, $secret);
-    $config = new Config(__DIR__ . '/../config/ooapi.ini');
-    $credentialStorage = new CredentialStorage($config->getCredentialDir());
-    $credentialStorage->activateEncryption(new EncrypterOpenSSL($password));
-    $credentialStorage->save($credentials);
+$message = null;
 
-    echo json_encode(['message' => 'api-credentials stored encrypted...']);
-}
-else
+try
 {
-    echo json_encode(['message' => $token]);
-    //echo json_encode(['message' => 'please enter token, secret and password']);
+    if (is_string($token) && is_string($secret) && is_string($password))
+    {
+        $credentials = new Credentials($token, $secret);
+        $config = new Config(__DIR__ . '/../config/ooapi.ini');
+        $credentialStorage = new CredentialStorage($config->getCredentialDir());
+        $credentialStorage->activateEncryption(new EncrypterOpenSSL($password));
+        $credentialStorage->save($credentials);
+
+        $message = 'api-credentials stored encrypted...';
+    }
+    else
+    {
+        $message = 'please enter token and secret';
+    }
 }
+catch (EmptyPasswordException $exception)
+{
+    $message = 'password must not be empty';
+}
+
+echo json_encode(['message' => $message]);
 
