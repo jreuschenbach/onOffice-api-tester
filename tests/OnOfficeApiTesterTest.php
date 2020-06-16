14cb2f553ac573139fe613d93fba36b3525e0fe7
jreuschenbach <j.reuschenbach@onOffice.com>
2020-06-16 20:56:07 +0200

    documentation OnOfficeApiTesterTest.php

--- tests/OnOfficeApiTesterTest.php
+++ tests/OnOfficeApiTesterTest.php
@@ -6,6 +6,14 @@ use jr\ooapi\api\ApiResponse;
 use jr\ooapi\CredentialStorage;
 use jr\ooapi\api\ApiRequest;
 
+/**
+ * Class OnOfficeApiTesterTest
+ *
+ * @covers \jr\ooapi\OnOfficeApiTester
+ * @uses \jr\ooapi\Config
+ *
+ */
+
 class OnOfficeApiTesterTest extends TestCase
 {
     const JSON = '{"actionid":"urn:onoffice-de-ns:smart:2.5:smartml:action:read","resourceid":"resource-id","resourcetype":"estate","identifier":"","timestamp":1589567897,"hmac":"88462bce11c5c47fb738dba64a36ba00","parameters":{"data":["Id", "kaufpreis", "lage"]}}';
